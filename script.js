9c5b0b0f8b1af16adb4e1088213a4c5040be4997
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-19 10:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Your script here.
+function startSpeaking() {
+    const text = document.getElementByTagName('textarea').value;
+    msg.text = text;
+    voicesDropdown.value;
+    msg.voice = speechSynthesis.getVoices().find(voice => voice.name == voicesDropdown);
+	const rate = parseFloat(options.value);
+    msg.rate = rate;
+	const pitch = parseFloat(options.value);
+    msg.pitch = pitch;
+    speechSynthesis.speak(msg);
+}
+
+function stopSpeaking() {
+    speechSynthesis.cancel();
+}
+speakButton.addEventListener('click', startSpeaking);
+stopButton.addEventListener('click', stopSpeaking);
